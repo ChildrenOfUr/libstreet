30087ebb55b57e5e0fa15c3fccf894bc15b7ab4a
Robert McDermot <robert.mcdermot@gmail.com>
2015-05-17 20:17:24 -0400

    tried to make it a little more accurate

--- lib/libstreet.dart
+++ lib/libstreet.dart
@@ -5,197 +5,171 @@ import 'dart:math' as Math;
 ResourceManager RESOURCES;
 Stage STAGE;
 
-
-
-
-
-
-
-
 /// TODO We'll use the Street and StreetLayer classes
 /// to proxy the DisplayObject containers instead of extending them
 /// This will prevent us from getting confused by the four different x variables.
 
+class Camera {
+	num _x = 0, _y = 0;
 
+	num get x => _x;
 
+	num get y => _y;
 
+	void set x(num newX) {
+//		if(newX < 0)
+//			newX = 0;
+		_x = newX;
+	}
 
+	void set y(num newY) {
+//		if(newY < 0)
+//			newY = 0;
+		_y = newY;
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	String toString() => "Camera - x: $_x, y:$_y";
+}
 
 class Street extends DisplayObjectContainer {
-  Map _streetDef;
+	Map _streetDef;
+	Camera camera = new Camera();
 
-  String get label => _streetDef['label'];
-  int get groundY => -(_streetDef['dynamic']['ground_y'] as int).abs();
+	String get label => _streetDef['label'];
 
+	int get groundY => -(_streetDef['dynamic']['ground_y'] as int).abs();
 
-  Rectangle get streetBounds => new Rectangle(
-      _streetDef['dynamic']['l'],
-      _streetDef['dynamic']['t'],
-      (_streetDef['dynamic']['l'].abs() + _streetDef['dynamic']['r'].abs()).toInt(),
-      (_streetDef['dynamic']['t'].abs() + _streetDef['dynamic']['b'].abs()).toInt()
-  );
+	Rectangle get streetBounds => new Rectangle(
+		_streetDef['dynamic']['l'],
+		_streetDef['dynamic']['t'],
+		(_streetDef['dynamic']['l'].abs() + _streetDef['dynamic']['r'].abs()).toInt(),
+		(_streetDef['dynamic']['t'].abs() + _streetDef['dynamic']['b'].abs()).toInt()
+		);
 
-  num camera_x = 0;
-  num camera_y = 0;
-  setCamera(num x, num y) {
-    camera_x = x;
-    camera_y = y;
-    update();
-  }
+	Street(final this._streetDef);
 
+	load() async {
+		await loadDecos();
 
-  Street(final this._streetDef);
+		// Gradient Layer
+		addChild(gradient);
 
-  load() async {
-    await loadDecos();
+		// Sort by z values
+		List layerList = new List.from(_streetDef['dynamic']['layers'].values)
+			..sort((Map A, Map B) => A['z'].compareTo(B['z']));
 
-    // Gradient Layer
-    addChild(gradient());
+		for(Map layerDef in layerList) {
+			Sprite layer = new StreetLayer(layerDef, streetBounds, groundY);
+			addChild(layer);
+		}
+	}
 
-    // Sort by z values
-    List layerList = new List.from(_streetDef['dynamic']['layers'].values)
-      ..sort((Map A, Map B) => A['z'].compareTo(B['z']));
+	/// Returns a DisplayObject containing the background gradient of the street.
+	DisplayObject get gradient {
+		var shape = new Shape();
+		shape.graphics.rect(0, 0, streetBounds.width, streetBounds.height);
+		shape.graphics.fillGradient(
+			new GraphicsGradient.linear(0, 0, 0, streetBounds.height)
+				..addColorStop(0, int.parse('0xFF' + _streetDef['gradient']['top']))
+				..addColorStop(1, int.parse('0xFF' + _streetDef['gradient']['bottom']))
+			);
+		shape.applyCache(0, 0, streetBounds.width, streetBounds.height);
+		return shape;
+	}
 
-    for (Map layerDef in layerList) {
-      Sprite layer = new StreetLayer(layerDef, streetBounds, groundY);
-      addChild(layer);
+	/// Completes when each deco is loaded into memory.
+	loadDecos() async {
+		var loadOptions = new BitmapDataLoadOptions()
+			..corsEnabled = true;
 
-    }
+		// Collect the url of each deco to load.
+		for(Map layer in _streetDef['dynamic']['layers'].values) {
+			for(Map deco in layer['decos']) {
+				if(!RESOURCES.containsBitmapData(deco['filename'])) {
+					RESOURCES.addBitmapData(
+						deco['filename'],
+						'http://childrenofur.com/locodarto/scenery/' + deco['filename'] + '.png',
+						loadOptions
+						);
+				}
+			}
+		}
+		await RESOURCES.load();
+		if(RESOURCES.failedResources.isNotEmpty)
+			throw('failed to download decos ${RESOURCES.failedResources}');
+	}
 
-  }
-
-  /// Returns a DisplayObject containing the background gradient of the street.
-  DisplayObject gradient() {
-    var shape = new Shape();
-    shape.graphics.rect(0, 0, streetBounds.width, streetBounds.height);
-    shape.graphics.fillGradient(
-        new GraphicsGradient.linear(0, 0, 0, streetBounds.height)
-          ..addColorStop(0, int.parse('0xFF' + _streetDef['gradient']['top']))
-          ..addColorStop(1, int.parse('0xFF' + _streetDef['gradient']['bottom']))
-    );
-    shape.applyCache(0,0, streetBounds.width, streetBounds.height);
-    return shape;
-  }
-
-
-  /// Completes when each deco is loaded into memory.
-  loadDecos() async {
-
-    var loadOptions = new BitmapDataLoadOptions()
-      ..corsEnabled = true;
-
-    // Collect the url of each deco to load.
-    for (Map layer in _streetDef['dynamic']['layers'].values)
-    {
-      for (Map deco in layer['decos'])
-      {
-        if ( !RESOURCES.containsBitmapData(deco['filename']) )
-          RESOURCES.addBitmapData(
-              deco['filename'],
-            'http://childrenofur.com/locodarto/scenery/' + deco['filename'] + '.png',
-            loadOptions
-          );
-      }
-    }
-    await RESOURCES.load();
-    if (RESOURCES.failedResources.isNotEmpty)
-      throw('failed to download decos ${RESOURCES.failedResources}');
-  }
-
-  update() {
-
-    x= camera_x;
-    y= camera_y;
-
-
-    List layers = children.where((E) => E is StreetLayer);
-    for (StreetLayer layer in layers) {
-      layer.update();
-    }
-  }
-
+	update() {
+		List layers = children.where((E) => E is StreetLayer).toList();
+		for(StreetLayer layer in layers) {
+			layer.update(camera);
+		}
+	}
 }
 
 class StreetLayer extends Sprite {
-  Map layerDef;
-  int groundY;
-  Rectangle streetBounds;
-  StreetLayer(this.layerDef, this.streetBounds, this.groundY) {
+	Map layerDef;
+	int groundY;
+	Rectangle streetBounds;
 
-    // Sort by z values
-    List decoList = layerDef['decos']
-      ..sort((Map A, Map B) => A['z'].compareTo(B['z']));
+	StreetLayer(this.layerDef, this.streetBounds, this.groundY) {
+		// Sort by z values
+		List decoList = layerDef['decos']
+			..sort((Map A, Map B) => A['z'].compareTo(B['z']));
 
-    for (Map decoMap in decoList) {
-      // Assume the deco BitmapData is loaded into memory, [Street] should do this.
-      Bitmap b_deco = new Bitmap(RESOURCES.getBitmapData(decoMap['filename']));
+		for(Map decoMap in decoList) {
+			// Assume the deco BitmapData is loaded into memory, [Street] should do this.
+			Bitmap b_deco = new Bitmap(RESOURCES.getBitmapData(decoMap['filename']));
 
-      // debug
-      Sprite deco = new Sprite()
-        ..addChild(b_deco);
+			// debug
+			Sprite deco = new Sprite()
+				..addChild(b_deco);
 
-      deco.x = decoMap['x'] - decoMap['w'] ~/ 2;
-      deco.y = decoMap['y'] - decoMap['h'] + groundY;
+			deco.x = decoMap['x'] - decoMap['w'] ~/ 2;
+			deco.y = decoMap['y'] - decoMap['h'] + groundY;
 
-      if(layerDef['name'] == 'middleground')
-      {
-        //middleground has different layout needs
-        deco.y += layerDef['h'];
-        deco.x += layerDef['w'] ~/ 2;
-      }
+			if(layerDef['name'] == 'middleground') {
+				//middleground has different layout needs
+				deco.y += layerDef['h'];
+				deco.x += layerDef['w'] ~/ 2;
+			}
 
-      // Set width
-      if (decoMap['h_flip'] == true)
-        deco.width = -decoMap['w'];
-      else
-        deco.width = decoMap['w'];
-      // Set height
-      if (decoMap['v_flip'] == true)
-        deco.height = -decoMap['h'];
-      else
-        deco.height = decoMap['h'];
+			if(decoMap['h_flip'] == true) {
+				deco.width = -decoMap['w'];
+				deco.x += decoMap['w'];
+			} else {
+				deco.width = decoMap['w'];
+			}
+			if(decoMap['v_flip'] == true) {
+				deco.height = -decoMap['h'];
+				deco.y += decoMap['h'];
+			} else {
+				deco.height = decoMap['h'];
+			}
 
-      if (decoMap['r'] != null) {
-        deco.rotation = decoMap['r'] * Math.PI/180;
-      }
+			if(decoMap['r'] != null) {
+				deco.rotation = decoMap['r'] * Math.PI / 180;
+			}
 
-      deco.onMouseClick.listen((_) {
-        print(layerDef['name'] + ' ' + decoMap['filename']);
-      });
+			deco.onMouseClick.listen((_) {
+				print(layerDef['name'] + ' ' + decoMap['filename']);
+			});
 
-      // Add to the layer
-      this.addChild(deco);
-    }
+			// Add to the layer
+			this.addChild(deco);
+		}
+	}
 
-    update();
-  }
-  update() {
+	update(Camera camera) {
+		num currentPercentX = camera.x / (streetBounds.width - STAGE.stageWidth);
+		num currentPercentY = camera.y / (streetBounds.height - STAGE.stageHeight);
 
-    num currentPercentX = camera.getX() / (streetBounds.width - STAGE.stageWidth);
-    num currentPercentY = camera.getY() / (streetBounds.height - STAGE.stageHeight);
+		//modify left and top for parallaxing
+		num offsetX = (layerDef['w'] - STAGE.stageWidth) * currentPercentX;
+		num offsetY = (layerDef['h'] - STAGE.stageHeight) * currentPercentY;
+		offsetY += groundY;
 
-    //modify left and top for parallaxing
-    num offsetX = (layerDef['w'] - STAGE.stageWidth) * currentPercentX - (layerDef['w']/2);
-    num offsetY = (layerDef['h'] - STAGE.stageHeight) * currentPercentY - (layerDef['h']/2);
-    offsetY += groundY;
-
-    x = -offsetX;
-    y = -offsetY;
-
-  }
-
+		x = -offsetX;
+		y = -offsetY;
+	}
 }
--- web/main.dart
+++ web/main.dart
@@ -3,52 +3,44 @@ import 'package:stagexl/stagexl.dart';
 import 'dart:html';
 import 'dart:convert';
 
-
-
-
 main() async {
-  StageXL.stageOptions.renderEngine = RenderEngine.Canvas2D;
+	StageXL.stageOptions.renderEngine = RenderEngine.WebGL;
 
-  // Setting up the stageXL environment
-  RESOURCES = new ResourceManager();
-  STAGE = new Stage(querySelector('canvas'));
-  new RenderLoop()
-    ..addStage(STAGE);
+	// Setting up the stageXL environment
+	RESOURCES = new ResourceManager();
+	STAGE = new Stage(querySelector('canvas'));
+	new RenderLoop()
+		..addStage(STAGE);
 
-  print('${STAGE.stageWidth} ${STAGE.stageHeight}');
+	print('${STAGE.stageWidth} ${STAGE.stageHeight}');
 
+	// load the JSON
+	RESOURCES.addTextFile('groddle', 'groddle.json');
+	await RESOURCES.load();
 
-  // load the JSON
-  RESOURCES.addTextFile('groddle', 'groddle.json');
-  await RESOURCES.load();
+	// We turn the JSON into a map before we generate a street from it.
+	// This allows us to dynamically create Maps and render Streets
+	// from them without a JSON conversion process.
+	Map groddleDef = JSON.decode(RESOURCES.getTextFile('groddle'));
 
-  // We turn the JSON into a map before we generate a street from it.
-  // This allows us to dynamically create Maps and render Streets
-  // from them without a JSON conversion process.
-  Map groddleDef = JSON.decode(RESOURCES.getTextFile('groddle'));
+	// Render the Street
+	Street groddle = new Street(groddleDef);
+	STAGE.addChild(groddle);
 
-  // Render the Street
-  Street groddle = new Street(groddleDef);
-  STAGE.addChild(groddle);
+	await groddle.load();
 
-  await groddle.load();
+	document.onKeyPress.listen((event) {
+		if(event.keyCode == 97)
+			groddle.camera.x -= 30;
+		if(event.keyCode == 100)
+			groddle.camera.x += 30;
+		if(event.keyCode == 119)
+			groddle.camera.y -= 30;
+		if(event.keyCode == 115)
+			groddle.camera.y += 30;
 
-  document.onKeyPress.listen((event) {
-    if (event.keyCode == 65)
-      groddle.camera_x--;
-    if (event.keyCode == 68)
-      groddle.camera_x++;
-    if (event.keyCode == 87)
-      groddle.camera_y--;
-    if (event.keyCode == 83)
-      groddle.camera_y++;
-    groddle.update();
-  });
+		groddle.update();
+	});
 
-
-  groddle.update();
-
-
+	groddle.update();
 }
-
-
